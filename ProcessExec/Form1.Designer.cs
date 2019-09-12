1b4c48ece89ffdcf2da18c39bde532a9c6a53b0a
willyan fidelis <willyan.sergio.fidelis@gmail.com>
2019-09-11 21:51:14 -0300

    first commit

---
+++ ProcessExec/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ProcessExec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_test = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.txt_send = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_recive = new System.Windows.Forms.TextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(12, 12);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_test.TabIndex = 0;
+            this.btn_test.Text = "Start";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Location = new System.Drawing.Point(111, 12);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Kill";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(213, 12);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 2;
+            this.btn_exit.Text = "Stop";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(12, 113);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(101, 23);
+            this.btn_send.TabIndex = 3;
+            this.btn_send.Text = "Escrever";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // txt_send
+            // 
+            this.txt_send.Location = new System.Drawing.Point(12, 155);
+            this.txt_send.Name = "txt_send";
+            this.txt_send.Size = new System.Drawing.Size(757, 20);
+            this.txt_send.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Texto para enviar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Texto recebido";
+            // 
+            // txt_recive
+            // 
+            this.txt_recive.AllowDrop = true;
+            this.txt_recive.Enabled = false;
+            this.txt_recive.Location = new System.Drawing.Point(12, 224);
+            this.txt_recive.Multiline = true;
+            this.txt_recive.Name = "txt_recive";
+            this.txt_recive.Size = new System.Drawing.Size(757, 214);
+            this.txt_recive.TabIndex = 6;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(15, 182);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(98, 23);
+            this.btn_clear.TabIndex = 8;
+            this.btn_clear.Text = "Limpar Historico";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 450);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_recive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_send);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_test);
+            this.Name = "Form1";
+            this.Text = "Start Process";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Leave += new System.EventHandler(this.Form1_Leave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox txt_send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_recive;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ ProcessExec/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProcessExec
+{
+    public partial class Form1 : Form
+    {
+        RemoteProcessControl process;
+        private Thread threadProcess;
+        private delegate void SetTextCallback(string message);
+
+        private void Listen(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+
+            //txt_recive.Text = lastMsg; //Erro devido a multi-threads. Problema resolvido aqui:
+            //https://stackoverflow.com/questions/14226921/threading-copied-error-settextcallback
+            //http://www.macoratti.net/14/07/c_gptwf2.htm
+            if (this.txt_recive.InvokeRequired)
+            {
+                if (e.Data == null) { return; }
+                try
+                {
+                    SetTextCallback x = new SetTextCallback((texto) => { /*txt_recive.Text = texto;*/txt_recive.AppendText(texto); txt_recive.AppendText(Environment.NewLine); });
+                    this.Invoke(x, new object[] { e.Data });
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        public Form1()
+        {
+            //process = new RemoteProcessControl(@"node.exe", @"C:\Users\willyan\Documents\willyan\NodeJSWebDir\local-api\dist\index.js", Listen);
+            process = new RemoteProcessControl(@"node.exe", @"C:\mro\local-api\dist\index.js", Listen);
+            InitializeComponent();
+        }
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+            threadProcess = new Thread(new ThreadStart(process.Start));
+            threadProcess.Priority = ThreadPriority.Normal;
+            threadProcess.Start();
+        }
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            process.Kill();
+        }
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            process.Stop();
+        }
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            process.Write(txt_send.Text);
+        }
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_recive.Text = "";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                threadProcess = new Thread(new ThreadStart(process.Start));
+                threadProcess.Priority = ThreadPriority.Normal;
+                threadProcess.Start();
+            }
+            catch(Exception ex)
+            {
+                txt_recive.Text = ex.Message;
+            }
+        }
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            process.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //process.Stop();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            process.Stop();
+        }
+    }
+
+    public class RemoteProcessControl
+    {
+        //https://gist.github.com/elerch/5628117
+        private string cmd = "";
+        private string arg = "";
+        Process proc;
+        private DataReceivedEventHandler cb;
+
+        public RemoteProcessControl(string cmd, string arg, DataReceivedEventHandler cb)
+        {
+            this.cmd = cmd;
+            this.arg = arg;
+            this.cb = cb;
+        }
+
+        private void Init()
+        {
+            proc = new System.Diagnostics.Process();
+            proc.StartInfo.Verb = "runas";
+
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.FileName = cmd;
+            proc.StartInfo.Arguments = arg;
+        }
+
+        static void Listen(object sender, System.Diagnostics.DataReceivedEventArgs e)
+
+        {
+            Console.WriteLine("Hello: " + e.Data);
+        }
+
+        public void Write(string data)
+        {
+            try
+            {
+                proc.StandardInput.WriteLine(data);
+            }
+            catch(Exception ex)
+            {
+                //throw ex;
+            }
+        }
+
+        public void Start()
+        {
+            if(proc != null){ return; }
+            Init();
+            proc.Start();
+            proc.BeginOutputReadLine();
+            //proc.OutputDataReceived += Listen;
+            proc.OutputDataReceived += cb;
+            proc.WaitForExit();
+
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                Write("STOP_CMD");
+                proc = null;
+            }
+            catch(Exception ex)
+            {
+                //throw ex;
+            }
+        }
+
+        public void Kill()
+        {
+            try
+            {
+                proc.Kill();
+                proc = null;
+            }
+            catch (Exception ex)
+            {
+                //throw ex;
+            }
+
+        }
+
+    }
+
+}
